e44c12dd6f38eb11832adc3ead7f38d7907df922
root <root@DESKTOP-6MC7RVM>
2023-11-26 18:22:32 +0100

    tilføjet menu

--- install.sh
+++ install.sh
@@ -1,68 +1,42 @@
-#!/bin/bash
+source ./utils/menu.sh
+OPTIONS_VALUES=("Update" "Python" "Docker" "Neovim" "Lazyvim" "Git" "oh-my-posh")
+for i in "${!OPTIONS_VALUES[@]}"; do
+	OPTIONS_STRING+="${OPTIONS_VALUES[$i]};"
+done
 
-msg() {
-        echo -e "\e[32m ############################### $1 ###############################  \e[0m"
-}
+menu SELECTED "$OPTIONS_STRING"
 
-msg "Starting Update"
-sudo apt-get update
-
-msg "Starting Upgrade"
-sudo apt-get upgrade -y
-
-msg "Installing Docker"
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-##################
-msg "installing GIT"
-sudo apt-get install git
-##################
-msg "installing Github cli"
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
-        sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
-        sudo apt update &&
-        sudo apt install gh -y
-################
-msg "installing neovim"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-# Optional: exposing nvim globally.
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-rm -rf nvim.appimage
-#############
-msg "installing lazyvim"
-# required
-mv ~/.config/nvim{,.bak}
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
-############
-msg "installing Python"
-sudo apt install python3
-############
-msg "installing Oh-my-posh"
-sudo apt install unzip
-curl -s https://ohmyposh.dev/install.sh | bash -s
-if ! grep -q "oh-my-posh" ~/.bashrc; then
-    echo "eval \"\$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/capr4n.omp.json')\"" >> ~/.bashrc
-    echo "oh-my-posh line added to .bashrc"
-else
-    echo "oh-my-posh line already exists in .bashrc"
-fi
-
+source ./utils/installers.sh
+for i in "${!SELECTED[@]}"; do
+	if [ "${SELECTED[$i]}" == "true" ]; then
+		CHECKED+=("${OPTIONS_VALUES[$i]}")
+	fi
+done
+for item in "${CHECKED[@]}"; do
+    case "$item" in
+        "Python")
+          python_select 
+          ;;
+        "Update")
+          update_select  
+          ;;
+        "Docker")
+          docker_select
+          ;;
+          "Git")
+          git_select
+          ;;
+        "Neovim")
+          neovim_select
+          ;;
+        "Lazyvim")
+          lazyvim_select
+          ;;
+        "oh-my-posh")
+          ohmyposh_select
+          ;;
+        *)
+            # You can add other cases here
+            ;;
+    esac
+done
---
+++ utils/installers.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+update_select() {
+    echo "Starting Update"
+    sudo apt-get update
+    echo "Starting Upgrade"
+    sudo apt-get upgrade -y
+}
+
+python_select() {
+    sudo apt install python3
+}
+
+docker_select() {
+    # Remove existing Docker packages
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do 
+        sudo apt-get remove -y $pkg
+    done
+
+    # Add Docker's official GPG key and repository
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+git_select() {
+    sudo apt-get install git
+}
+
+git_cli_select() {
+    sudo apt-get update
+    sudo apt-get install -y curl
+
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+    sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install -y gh
+}
+
+neovim_select() {
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    sudo mv squashfs-root /
+    sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+    rm -rf nvim.appimage
+}
+
+lazyvim_select() {
+    mv ~/.config/nvim{,.bak}
+    git clone https://github.com/LazyVim/starter ~/.config/nvim
+    rm -rf ~/.config/nvim/.git
+}
+
+ohmyposh_select() {
+    sudo apt-get install -y unzip
+    curl -s https://ohmyposh.dev/install.sh | bash -s
+
+    if ! grep -q "oh-my-posh" ~/.bashrc; then
+        echo "eval \"\$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/capr4n.omp.json')\"" >> ~/.bashrc
+        echo "oh-my-posh line added to .bashrc"
+    else
+        echo "oh-my-posh line already exists in .bashrc"
+    fi
+}
